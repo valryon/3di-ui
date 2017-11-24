a0ed546d4be172339c56c6eeeffabf923c5a75a0
Damien Mayance <apps@mayance.io>
2017-11-24 10:45:09 +0100

    Game background

---
+++ Assets/scripts/game/GameScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+  #region Members
+
+  [Header("Gameplay")]
+  public GameObject[] objectsToShoot;
+
+  [Header("UI")]
+  public GameObject settings;
+
+  #endregion
+
+  #region Timeline
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    // Back from pause
+    if (settings.activeInHierarchy == false && Time.timeScale == 0)
+    {
+      Time.timeScale = 1f;
+    }
+  }
+
+  #endregion
+
+  #region Public methods
+
+  public void ShowSettings()
+  {
+    Time.timeScale = 0;
+    settings.SetActive(true);
+  }
+
+  public void BackToMenu()
+  {
+
+  }
+
+  #endregion
+}
---
+++ Assets/scripts/tools/Editor/TexturePostProcessor.cs
@@ -0,0 +1,38 @@
+﻿// This file is subject to the terms and conditions defined in
+// file 'LICENSE.md', which is part of this source code package.
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Change default image import settings
+/// </summary>
+public class TexturePostProcessor : AssetPostprocessor
+{
+  void OnPostprocessTexture(Texture2D texture)
+  {
+    TextureImporter importer = assetImporter as TextureImporter;
+
+    if (importer.assetPath.ToLower().Contains("sprites/"))
+    {
+      importer.textureType = TextureImporterType.Sprite;
+      importer.mipmapEnabled = false;
+      importer.spritePixelsPerUnit = 25;
+      Apply(importer, FilterMode.Point, TextureImporterCompression.Uncompressed);
+    }
+  }
+  
+  private void Apply(TextureImporter importer, FilterMode filterMode, TextureImporterCompression compression)
+  {
+    if (importer.filterMode != filterMode || importer.textureCompression != compression)
+    {
+      importer.filterMode = filterMode;
+      importer.textureCompression = compression;
+      importer.alphaIsTransparency = true;
+      importer.wrapMode = TextureWrapMode.Repeat;
+      importer.mipmapEnabled = false;
+
+      // Reimport.
+      AssetDatabase.ImportAsset(importer.assetPath);
+    }
+  }
+}
